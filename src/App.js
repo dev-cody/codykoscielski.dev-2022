c5e4f851e6176acd8e89d58ffbe0e1510b5f5546
Cody Koscielski <codykoscielski@gmail.com>
2022-09-13 08:07:15 -0400

    Added Projects component.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
 import Tech from './components/Tech'
+import Projects from './components/Projects'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <About />
       <Tech />
+      <Projects />
     </div>
   );
 }
