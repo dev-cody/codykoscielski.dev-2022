210f0b17538c6f348725dcd5630aa4632555dd1f
Cody Koscielski <codykoscielski@gmail.com>
2022-09-13 07:44:27 -0400

    Added the tech icons, styled the tech page

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import  HTML  from '../assets/html.svg'
+import css from '../assets/css.svg'
+import js from '../assets/js.svg'
+import bootstrap from '../assets/bootstrap.svg'
+import database from '../assets/database.svg'
+import react from '../assets/react.svg'
 
 const Tech = () => {
   return (
@@ -10,7 +16,34 @@ const Tech = () => {
                     <h2 className='text-xl font-bold ml-[-15px] mt-[-40px]'>tools.</h2>
                 </div>
             </div>
-            <h2 className='text-40 leading-10'>some of the <span className='font-bold'>cool</span> tech i <span className='font-bold'>work</span> with<span className='text-red'>.</span> </h2>
+            <h2 className='text-40 leading-10'>here is some of the <span className='font-bold'>cool</span> tech i <span className='font-bold'>work</span> with<span className='text-red'>.</span> </h2>
+            <div className="flex flex-wrap justify-evenly items-center my-4">
+              <div className='flex flex-col items-center justify-center'>
+                <img src={ HTML } alt="html logo" />
+                <p className='text-md font-bold'>html<span className='text-red'>5</span></p>
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <img src={ css } alt="html logo" />
+                <p className='text-md font-bold'>cs<span className='text-red'>s</span>3</p>
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <img src={ js } alt="html logo" />
+                <p className='text-md font-bold'>javasc<span className='text-red'>r</span>ipt</p>
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <img src={ react } alt="html logo" />
+                <p className='text-md font-bold'>react<span className='text-red'>.</span>js</p>
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <img src={ bootstrap } alt="html logo" />
+                <p className='text-md font-bold'>bo<span className='text-red'>o</span>tstrap</p>
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <img src={ database } alt="html logo" />
+                <p className='text-md font-bold'>mys<span className='text-red'>q</span>l</p>
+              </div>
+            </div>
+            <p className='text-md'>i am always keeping up and trying to learn some of the more up to date technologies<span className='text-red'>.</span></p>
         </div>
     </div>
   )
