9d60741a5e4438f22049c33da508ce37fe2ed7fa
Cody Koscielski <codykoscielski@gmail.com>
2022-09-15 06:56:29 -0400

    Adjusted the styling for the tech section.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,26 +12,28 @@ const Navbar = () => {
     }
 
     return (
-    <nav className='flex justify-between items-center h-24 max-w-[1240px] mx-auto p-4 sticky top-0 z-50 bg-dark'>
-        <a href="#home"><img src={ Logo } alt="codykoscielski.dev Logo" /></a>
-        <ul className='hidden md:flex'>
-            <a href="#home"><li className='p-4 text-white text-md'>home</li></a>
-            <a href="#about"><li className='p-4 text-white text-md'>about</li></a>
-            <a href='#tech'><li className='p-4 text-white text-md'>tech</li></a>
-            <li className='p-4 text-white text-md'>projects</li>
-            <li className='p-4 text-white text-md'>lets chat</li>
-        </ul>
-        <div onClick={handleNav} className='block md:hidden'>
-            {nav ? <AiOutlineClose size={40} color="#E76F51"/> : <AiOutlineMenu size={40} color="#E76F51"/>}
+    <nav className='w-full top-0 z-50 bg-dark sticky'>
+        <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto p-4  '>
+            <a href="#home"><img src={ Logo } alt="codykoscielski.dev Logo" /></a>
+            <ul className='hidden md:flex'>
+                <a href="#home"><li className='p-4 text-white text-md'>home</li></a>
+                <a href="#about"><li className='p-4 text-white text-md'>about</li></a>
+                <a href='#tech'><li className='p-4 text-white text-md'>tech</li></a>
+                <li className='p-4 text-white text-md'>projects</li>
+                <li className='p-4 text-white text-md'>lets chat</li>
+            </ul>
+            <div onClick={handleNav} className='block md:hidden'>
+                {nav ? <AiOutlineClose size={40} color="#E76F51"/> : <AiOutlineMenu size={40} color="#E76F51"/>}
+            </div>
+            <ul className={nav ? 'fixed left-0 top-0 w-[75%] h-full border-r bg-red ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+                <img src={ Logo } className="p-4" alt="codykoscielski.dev Logo" />
+                <a href="#home"><li className='p-4 border-b border-gray-600 text-white text-md'>home</li></a>
+                <a href="#about"><li className='p-4 border-b border-gray-600 text-white text-md'>about</li></a>
+                <a href='#tech'><li className='p-4 border-b border-gray-600 text-white text-md'>tech</li></a>
+                <li className='p-4 border-b border-gray-600 text-white text-md'>projects</li>
+                <li className='p-4 text-white text-md'>lets chat</li>
+            </ul>
         </div>
-        <ul className={nav ? 'fixed left-0 top-0 w-[75%] h-full border-r bg-red ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
-            <img src={ Logo } className="p-4" alt="codykoscielski.dev Logo" />
-            <a href="#home"><li className='p-4 border-b border-gray-600 text-white text-md'>home</li></a>
-            <a href="#about"><li className='p-4 border-b border-gray-600 text-white text-md'>about</li></a>
-            <a href='#tech'><li className='p-4 border-b border-gray-600 text-white text-md'>tech</li></a>
-            <li className='p-4 border-b border-gray-600 text-white text-md'>projects</li>
-            <li className='p-4 text-white text-md'>lets chat</li>
-        </ul>
     </nav>
     )
 }
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -8,7 +8,7 @@ import react from '../assets/react.svg'
 
 const Tech = () => {
   return (
-    <div id='tech' className='text-white'>
+    <section id='tech' className='text-white'>
         <div className="triangle"></div>
         <div className="max-w-[800px] w-full mx-auto flex flex-col justify-start p-3 leading-tight">
             <div className='w-full flex justify-end'>
@@ -17,35 +17,41 @@ const Tech = () => {
                 </div>
             </div>
             <h2 className='text-40 leading-10'>here is some of the <span className='font-bold'>cool</span> tech i <span className='font-bold'>work</span> with<span className='text-red'>.</span> </h2>
-            <div className="flex flex-wrap justify-evenly items-center my-4">
-              <div className='flex flex-col items-center justify-center'>
-                <img src={ HTML } alt="html logo" />
-                <p className='text-md font-bold'>html<span className='text-red'>5</span></p>
+            <div className="flex flex-col justify-evenly items-center my-8">
+              <div className="flex flex-row justify-between w-[75%] items-center mb-10 md:w-[50%]">
+                <div className='flex flex-col items-center justify-center'>
+                  <img src={ HTML } alt="html logo" />
+                  <p className='text-md font-bold'>html<span className='text-red'>5</span></p>
+                </div>
+                <div className='flex flex-col items-center justify-center'>
+                  <img src={ css } alt="html logo" />
+                  <p className='text-md font-bold'>cs<span className='text-red'>s</span>3</p>
+                </div>
               </div>
-              <div className='flex flex-col items-center justify-center'>
-                <img src={ css } alt="html logo" />
-                <p className='text-md font-bold'>cs<span className='text-red'>s</span>3</p>
+              <div className="flex flex-row justify-between w-[75%] items-center mb-10 md:w-[50%]">
+                <div className='flex flex-col items-center justify-center'>
+                  <img src={ js } alt="html logo" />
+                  <p className='text-md font-bold'>javasc<span className='text-red'>r</span>ipt</p>
+                </div>
+                <div className='flex flex-col items-center justify-center'>
+                  <img src={ react } alt="html logo" />
+                  <p className='text-md font-bold'>react<span className='text-red'>.</span>js</p>
+                </div>
               </div>
-              <div className='flex flex-col items-center justify-center'>
-                <img src={ js } alt="html logo" />
-                <p className='text-md font-bold'>javasc<span className='text-red'>r</span>ipt</p>
-              </div>
-              <div className='flex flex-col items-center justify-center'>
-                <img src={ react } alt="html logo" />
-                <p className='text-md font-bold'>react<span className='text-red'>.</span>js</p>
-              </div>
-              <div className='flex flex-col items-center justify-center'>
-                <img src={ bootstrap } alt="html logo" />
-                <p className='text-md font-bold'>bo<span className='text-red'>o</span>tstrap</p>
-              </div>
-              <div className='flex flex-col items-center justify-center'>
-                <img src={ database } alt="html logo" />
-                <p className='text-md font-bold'>mys<span className='text-red'>q</span>l</p>
+              <div className="flex flex-row justify-between w-[75%] items-center md:w-[50%]">
+                <div className='flex flex-col items-center justify-center'>
+                  <img src={ bootstrap } alt="html logo" />
+                  <p className='text-md font-bold'>bo<span className='text-red'>o</span>tstrap</p>
+                </div>
+                <div className='flex flex-col items-center justify-center'>
+                  <img src={ database } alt="html logo" />
+                  <p className='text-md font-bold'>mys<span className='text-red'>q</span>l</p>
+                </div>
               </div>
             </div>
             <p className='text-md'>i am always keeping up and trying to learn some of the more up to date technologies<span className='text-red'>.</span></p>
         </div>
-    </div>
+    </section>
   )
 }
 
